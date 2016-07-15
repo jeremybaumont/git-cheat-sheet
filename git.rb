3f84e1f6311da59da05da2cb7b1043dba9580ce0
Lorin Hochstein <lhochstein@netflix.com>
2016-07-15 14:42:03 -0700

    Initial commit

---
+++ git.rb
@@ -0,0 +1,17 @@
+cheatsheet do
+    title 'Git'
+    docset_file_name 'Git'
+    keyword 'git'
+    category do
+        id 'stash'
+        entry do
+            name 'Stash untracked files'
+            notes <<-'END'
+            ```
+            git stash save -u
+            ```
+            END
+        end
+    end
+end
+
